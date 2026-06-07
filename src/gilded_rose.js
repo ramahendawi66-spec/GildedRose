8a64b070fde7948884dfcea38d85828da6a6300f
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-07 16:29:43 +0200

    Refactor: simplify update_quality with cleaner code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,53 +15,36 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-          if (items[i].name == 'Conjured Mana Cake') {
-            if (items[i].quality > 0) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+    var item = items[i];
+
+    if (item.name == 'Sulfuras, Hand of Ragnaros') {
+      continue;
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
+
+    item.sell_in = item.sell_in - 1;
+
+    if (item.name == 'Aged Brie') {
+      item.quality = item.sell_in < 0 ? item.quality + 2 : item.quality + 1;
+
+    } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.sell_in < 0) {
+        item.quality = 0;
+      } else if (item.sell_in < 5) {
+        item.quality = item.quality + 3;
+      } else if (item.sell_in < 10) {
+        item.quality = item.quality + 2;
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
+        item.quality = item.quality + 1;
       }
+
+    } else if (item.name == 'Conjured Mana Cake') {
+      item.quality = item.sell_in < 0 ? item.quality - 4 : item.quality - 2;
+
+    } else {
+      item.quality = item.sell_in < 0 ? item.quality - 2 : item.quality - 1;
     }
+
+    if (item.quality > 50) item.quality = 50;
+    if (item.quality < 0) item.quality = 0;
   }
 }
