af8bf959271b8e33ef69efd65356815632aabeea
Rama Mahmoud <if25b318@technikum-wien.at>
2026-06-07 16:11:36 +0200

    Green: Aged Brie quality increases by 2 after sell date

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -34,4 +34,46 @@ it("should increase quality of Aged Brie by 1 each day", function() {
   expect(items[0].quality).toEqual(21); //steigt um 1 
 });
 
+it("should increase quality of Aged Brie by 2 after sell date", function() {
+  items = [new Item("Aged Brie", -1, 20)];
+  update_quality();
+  expect(items[0].quality).toEqual(22);
+});
+
+it("should never change quality of Sulfuras", function() {
+  items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)];
+  update_quality();
+  expect(items[0].quality).toEqual(80);
+});
+
+it("should never change sell_in of Sulfuras", function() {
+  items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)];
+  update_quality();
+  expect(items[0].sell_in).toEqual(0);
+});
+
+it("should increase Backstage pass quality by 1 when sell_in > 10", function() {
+  items = [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)];
+  update_quality();
+  expect(items[0].quality).toEqual(21);
+});
+
+it("should increase Backstage pass quality by 2 when sell_in <= 10", function() {
+  items = [new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)];
+  update_quality();
+  expect(items[0].quality).toEqual(22);
+});
+
+it("should increase Backstage pass quality by 3 when sell_in <= 5", function() {
+  items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)];
+  update_quality();
+  expect(items[0].quality).toEqual(23);
+});
+
+it("should drop Backstage pass quality to 0 after concert", function() {
+  items = [new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)];
+  update_quality();
+  expect(items[0].quality).toEqual(0);
+});
+
 });
